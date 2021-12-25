30d637796bc2f3e618ae330b7cd3ee8646e57561
Md. Mazharul Islam <mazharulislam611@gmail.com>
2021-12-25 23:33:30 +0600

    create pages

---
+++ src/Components/Home/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='container mx-auto'>
+            <h1>Hello</h1>
+        </div>
+    );
+};
+
+export default Home;
