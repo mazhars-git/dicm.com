18a2b80d36f772629d8c337a7b8e89239c4419be
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-01-03 23:03:12 +0600

    tailwind menu added

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import logo from './../../../images/Logo.jpg';
+import MainMenu from './../MainMenu/MainMenu';
 
 const Home = () => {
     return (
         <div className='container mx-auto'>
+            <MainMenu />
+            <img className='w-16' src={logo}/>
             <h1 className='text-center'>Hello</h1>
 
             <button className="btn-primary">
