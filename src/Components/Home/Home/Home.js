0d477cf40827a6be613dd832789060f62028b5be
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-02-20 23:57:40 +0600

    swiper slide added

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Slider from '../../Slider/Slider';
 import MainMenu from './../MainMenu/MainMenu';
 
 const Home = () => {
     return (
         <div className='container mx-auto'>
             <MainMenu />
+            <Slider />
 
             <button className="btn-primary">
                 Save changes
