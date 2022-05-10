7f4d0b0656b2f07472ea48ca7869f766d61b56ef
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-05-10 23:59:20 +0600

    footer added

--- src/Components/Home/Footer/Footer.js
+++ src/Components/Home/Footer/Footer.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='container'>
-            <div className='row py-5'>
-                <h1>Footer Area</h1>
+        <div className='section-footer'>
+            <div className='container'>
+                <div className='row py-5'>
+                    <h1>Footer Area</h1>                
+                </div>
             </div>
-            
+            <div className='py-5'>
+                <p>Copyright &copy; 2022, All rights reserved Dhaka Ideal Citizen Madrasah</p>
+            </div>
         </div>
     );
 };
