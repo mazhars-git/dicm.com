36fa3bd6f1351528b634704baec2bf8079be6e55
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-05-10 23:53:27 +0600

    update

--- src/Components/Home/Footer/Footer.js
+++ src/Components/Home/Footer/Footer.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer Area</h1>
+        <div className='container'>
+            <div className='row py-5'>
+                <h1>Footer Area</h1>
+            </div>
+            
         </div>
     );
 };
--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Slider from '../../Slider/Slider';
 import MainMenu from './../MainMenu/MainMenu';
+import Footer from './../Footer/Footer';
 
 const Home = () => {
     return (
         <div className='container mx-auto'>
             <MainMenu />
             <Slider />
-
+            
             <button className="btn-primary">
                 Save changes
             </button>
+            <Footer />
         </div>
     );
 };
