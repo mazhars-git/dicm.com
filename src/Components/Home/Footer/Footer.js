6d8446855fe25018bb4a782c72afa6e401cc8916
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-10-09 22:58:30 +0600

    update

--- src/Components/Home/Footer/Footer.js
+++ src/Components/Home/Footer/Footer.js
@@ -1,31 +1,36 @@
 import React from 'react';
 import "../../Css/Footer.css";
 import logo from "../../../images/Logo.jpg";
+import Grid from '@material-ui/core/Grid';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { twitter, faFacebook, linkedin, faCoffee, faCake, faMailBulk, faLaptopCode } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
     return (
         <div className='section-footer'>
-            <div className='container'>
-                <h1>Footer Area</h1>                
+            <Grid container>
+                <Grid item xs={12}>
+                    <h1>Footer Area</h1>
+                </Grid>  
                 
-                <div className='row'>
-                    <div className='col-md-6'>
-                        <img width={100} src={logo} alt="logo"/>
+                <Grid item xs={12} sm={6}>
+                    <img width={100} src={logo} alt="logo"/>
 
-                        <ul>
-                            <li><a href="www.facebook.com"><FontAwesomeIcon icon={faCake} /></a></li>
-                            <li><a href="www.facebook.com"><FontAwesomeIcon icon={faMailBulk} /></a></li>
-                        </ul>
-                    </div>
-                    <div className='col-md-6'>
-
-                    </div>
-                </div>
-
-
-            </div>
+                    <ul>
+                        <li><a href="www.facebook.com"><FontAwesomeIcon icon={faCake} /></a></li>
+                        <li><a href="www.facebook.com"><FontAwesomeIcon icon={faMailBulk} /></a></li>
+                    </ul>
+                </Grid>
+                
+                <Grid item xs={12} sm={6}>
+                    <ul>
+                        <li><a href='#'>About</a></li>
+                        <li><a href='#'>Location</a></li>
+                        <li><a href='#'>Contact</a></li>
+                        <li><a href='#'>Carrer</a></li>
+                    </ul>
+                </Grid>
+            </Grid>
             <div className='py-5 footer-bottom'>
                 <p>Copyright &copy; 2022, All rights reserved Dhaka Ideal Citizen Madrasah</p>
             </div>
